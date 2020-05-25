39b8b51fe80f8b1024b916b034025f7ce1ec3c03
EddyHamzich <poorgamerno1@gmail.com>
2020-05-25 13:32:24 +0100

    home page updates

--- DatingApp_Frontend/app/src/App.js
+++ DatingApp_Frontend/app/src/App.js
@@ -8,11 +8,16 @@ export default class App extends Component {
             <div className="app">
                 <div className="navbar">
                     <h1>Timder</h1>
-                    <div className="nav-inputs">
+                    <ul>
+                        <li><a href="#">Matches</a></li>
+                        <li><a href="#">Lists</a></li>
+                        <li><a href="#">Messages</a></li>
+                    </ul>
+                    <form className="nav-inputs">
                         <input placeholder="Username" type="email" name="" id=""/>
                         <input placeholder="Password" type="password" name="" id=""/>
-                        <button>Login</button>
-                    </div>
+                        <button type="submit">Login</button>
+                    </form>
                 </div>
             </div>
         )
