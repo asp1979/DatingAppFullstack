6654bb1a5393d893478ed3ede2288e12e9b885b1
EddyHamzich <poorgamerno1@gmail.com>
2020-06-07 17:11:05 +0100

    changed username validation string length

--- DatingApp_API/DTOs/UserForRegisterDto.cs
+++ DatingApp_API/DTOs/UserForRegisterDto.cs
@@ -8,7 +8,7 @@ namespace DatingApp_API.DTOs
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "Please specify passwrod between 4 and 8 characters")]
+        [StringLength(16, MinimumLength = 4, ErrorMessage = "Please specify passwrod between 4 and 16 characters")]
         public string Password { get; set; }
     }
 }
