6a50069b7c021327463c922f3fb4f06f12fa90f2
EddyHamzich <poorgamerno1@gmail.com>
2020-06-27 18:36:15 +0100

    added Matches component + CSS cleanup

--- DatingApp_Frontend/app/src/components/Matches/Matches.jsx
+++ DatingApp_Frontend/app/src/components/Matches/Matches.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import './Matches.css';
+import React, { useState, useEffect, useContext } from 'react';
+import { UserContext } from '../../UserContext';
 
 export const Matches = () => {
+
+    const { userContext } = useContext(UserContext);
+    const [matches, setMatches] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function getMatches() {
+            const get = await fetch("http://localhost:5000/api/v1/users", {
+                headers: { "Authorization": "Bearer " + userContext.jwt }
+            });
+
+            if(get.ok) {
+                const res = await get.json();
+                setMatches(res);
+                setLoading(false);
+            } else {
+                console.log(get.status, "Error");
+            }
+        }
+        getMatches();
+        // eslint-disable-next-line
+    }, []);
+
     return (
         <div className="page matches">
             <div className="content">
                 <h1>Matches</h1>
+                <ul>
+                { 
+                    !loading && matches
+                    .filter(x => x.username !== userContext.jwtUsername)
+                    .map((match, i) => 
+                        <li className="person-card" key={i}>
+                            <p>{match.knownAs}</p>
+                            <img src={match.photoUrl} alt=""></img>
+                        </li>
+                    )
+                }
+                </ul>
             </div>
         </div>
     )
