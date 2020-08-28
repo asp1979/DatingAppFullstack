706bf39b4f100efaaaf31b8e7c1adea07160270a
EddyHamzich <poorgamerno1@gmail.com>
2020-08-28 21:52:58 +0100

    added new model Like + migrated

--- DatingApp_API/Data/DataContext.cs
+++ DatingApp_API/Data/DataContext.cs
@@ -14,6 +14,25 @@ namespace DatingApp_API.Data
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Like>()
+                .HasKey(u => new {u.LikerID, u.LikeeID});
+
+            builder.Entity<Like>()
+                .HasOne(u => u.Likee)
+                .WithMany(u => u.Likers)
+                .HasForeignKey(u => u.LikeeID)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            builder.Entity<Like>()
+                .HasOne(u => u.Liker)
+                .WithMany(u => u.Likees)
+                .HasForeignKey(u => u.LikerID)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
         
     }
 }
--- DatingApp_API/Models/User.cs
+++ DatingApp_API/Models/User.cs
@@ -19,5 +19,7 @@ namespace DatingApp_API.Models
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
         public ICollection<Photo> Photos { get; set; }
+        public ICollection<Like> Likers { get; set; }
+        public ICollection<Like> Likees { get; set; }
     }
 }
