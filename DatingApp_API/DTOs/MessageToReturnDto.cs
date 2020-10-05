f3eed2613f8526eb51cfc676dfcf1e0fda1d0acf
EddyHamzich <poorgamerno1@gmail.com>
2020-10-05 11:30:09 +0100

    changed logging information

--- DatingApp_API/DTOs/MessageToReturnDto.cs
+++ DatingApp_API/DTOs/MessageToReturnDto.cs
@@ -6,11 +6,9 @@ namespace DatingApp_API.DTOs
     {
         public int ID { get; set; }
         public int SenderID { get; set; }
-        public string Sender { get; set; }
         public string SenderUsername { get; set; }
         public string SenderPhotoUrl { get; set; }
         public int RecipientID { get; set; }
-        public string Recipient { get; set; }
         public string RecipientUsername { get; set; }
         public string RecipientPhotoUrl { get; set; }
         public string Content { get; set; }
