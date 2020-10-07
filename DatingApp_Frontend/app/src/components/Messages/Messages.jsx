2d73e7d63f003b1dd02abf75f7abf3fb9967cda8
EddyHamzich <poorgamerno1@gmail.com>
2020-10-07 10:56:19 +0100

    added fetching to messages componnet

--- DatingApp_Frontend/app/src/components/Messages/Messages.jsx
+++ DatingApp_Frontend/app/src/components/Messages/Messages.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { UserContext } from '../../UserContext';
 
 export const Messages = () => {
+
+    const { userContext } = useContext(UserContext);
+    const [messages, setMessages] = useState([]);
+    const [threads, setThreads] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function getMessages() {
+            const get = await fetch(`http://localhost:5000/api/v1/users/${userContext.jwtID}/messages`, {
+                headers: { "Authorization": "Bearer " + userContext.jwt }
+            });
+            if(get.ok) {
+                const res = await get.json();
+                setMessages([...res]);
+                setThreads([...new Set(res.map(x => x.senderUsername))]);
+                setLoading(false);
+            } else {
+                console.log(get.status, "Error");
+            }
+        }
+        getMessages();
+        // eslint-disable-next-line
+    }, []);
+
     return (
         <div className="page messages">
             <div className="content">
                 <h1>Messages</h1>
+                <ul>
+                { 
+                    !loading && threads
+                    .map((thread, i) => 
+                        <p key={i}>{thread}</p>
+                    )
+                }
+                </ul>
             </div>
         </div>
     )
