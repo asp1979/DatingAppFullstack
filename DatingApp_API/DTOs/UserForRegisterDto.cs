0634db37850cd824e993f306285845b87f19bfbe
EddyHamzich <poorgamerno1@gmail.com>
2020-10-11 08:19:35 +0100

    fixed typo

--- DatingApp_API/DTOs/UserForRegisterDto.cs
+++ DatingApp_API/DTOs/UserForRegisterDto.cs
@@ -5,11 +5,11 @@ namespace DatingApp_API.DTOs
     public class UserForRegisterDto
     {
         [Required]
-        [StringLength(16, MinimumLength = 4, ErrorMessage = "Please specify passwrod between 4 and 16 characters")]
+        [StringLength(16, MinimumLength = 4, ErrorMessage = "Please specify password between 4 and 16 characters")]
         public string Username { get; set; }
 
         [Required]
-        [StringLength(32, MinimumLength = 4, ErrorMessage = "Please specify passwrod between 4 and 32 characters")]
+        [StringLength(32, MinimumLength = 4, ErrorMessage = "Please specify password between 4 and 32 characters")]
         public string Password { get; set; }
     }
 }
