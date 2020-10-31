48b33cf0c3df7bfa02d8f80e2dd2e00f83052e4f
EddyHamzich <poorgamerno1@gmail.com>
2020-10-31 18:26:51 +0100

    added conditional rendering to home page

--- DatingApp_Frontend/app/src/components/Home/Home.jsx
+++ DatingApp_Frontend/app/src/components/Home/Home.jsx
@@ -1,53 +1,70 @@
 import './Home.css';
-import React from 'react';
+import React, { useContext } from 'react';
 import { motion } from 'framer-motion';
+import { UserContext } from '../../UserContext';
 
 export const Home = () => {
+
+    const { userContext } = useContext(UserContext);
+
     return (
         <div className="page home">
 
-            <div className="animation-div">
+            { userContext.loggedIn === false
+                ? <div className="animation-div">
 
-                <motion.h1
-                className="motion-h1"
-                initial={{ opacity: 0, y: -400, scale: 0 }}
-                animate={{ opacity: 1, y: 0,    scale: 2 }}
-                transition={{ delay: 0.25, duration: 0.5 }}>
+                    <motion.h1
+                    className="motion-h1"
+                    initial={{ opacity: 0, y: -400, scale: 0 }}
+                    animate={{ opacity: 1, y: 0,    scale: 2 }}
+                    transition={{ delay: 0.25, duration: 0.5 }}>
 
-                    Welcome to Timder!<br/>
+                        Welcome to Timder!<br/>
 
-                    <motion.p
-                    className="motion-p"
-                    initial={{ opacity: 0, y: 200 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ delay: 0.5, duration: 0.5 }}>
-                        The love of your life is just a few clicks away
-                    </motion.p>
+                        <motion.p
+                        className="motion-p"
+                        initial={{ opacity: 0, y: 200 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ delay: 0.5, duration: 0.5 }}>
+                            The love of your life is just a few clicks away
+                        </motion.p>
 
-                    <div className="call-to-action">
+                        <div className="call-to-action">
 
-                        <motion.a
-                        href="/login"
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ delay: 1.00, duration: 0.5 }}>
-                            Login
-                        </motion.a>
+                            <motion.a
+                            href="/login"
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 1.00, duration: 0.5 }}>
+                                Login
+                            </motion.a>
 
-                        <motion.a
-                        href="/register"
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ delay: 1.00, duration: 0.5 }}>
-                            Register
-                        </motion.a>
+                            <motion.a
+                            href="/register"
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 1.00, duration: 0.5 }}>
+                                Register
+                            </motion.a>
 
-                    </div>
+                        </div>
 
-                </motion.h1>
+                    </motion.h1>
 
-            </div>
+                </div>
 
+                : <div className="animation-div">
+
+                    <motion.h1
+                    className="motion-h1"
+                    initial={{ opacity: 0, scale: 1.5 }}
+                    animate={{ opacity: 1, scale: 1.5 }}
+                    transition={{ delay: 0.25, duration: 1 }}>
+                        I hope you are enjoying Timder!<br/>
+                    </motion.h1>
+
+                </div>
+            }
         </div>
     )
 }
