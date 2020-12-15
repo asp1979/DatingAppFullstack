c41061254f03374414a4612c1bf4d80a69676f0e
EddyHamzich <poorgamerno1@gmail.com>
2020-12-15 19:56:10 +0100

    upgraded react versions

--- DatingApp_Frontend/app/src/components/Login/Login.jsx
+++ DatingApp_Frontend/app/src/components/Login/Login.jsx
@@ -30,7 +30,6 @@ export const Login = withRouter(({ history }) => {
                 loggedIn: true
             });
             history.push("/matches"); // redirect
-            window.location.reload();
         } else {
             setFetchError(true);
         }
