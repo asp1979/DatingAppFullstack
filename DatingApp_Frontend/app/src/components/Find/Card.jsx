b3a491b4e97b736d171c145816886f07e5ed9703
EddyHamzich <poorgamerno1@gmail.com>
2021-03-26 17:51:21 +0100

    improve swipe animation

--- DatingApp_Frontend/app/src/components/Find/Card.jsx
+++ DatingApp_Frontend/app/src/components/Find/Card.jsx
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Frame, useMotionValue, useTransform, useAnimation } from "framer";
 import { UserContext } from '../../UserContext';
 
 export const Card = ({ user }) => {
 
+    const [display, setDisplay] = useState(true);
     const { userContext, setUserContext } = useContext(UserContext);
     const baseURL = userContext.baseURL;
     const headers = { headers: { "Authorization": "Bearer " + userContext.jwt } };
@@ -21,26 +22,16 @@ export const Card = ({ user }) => {
         }
     }
 
-    // To move the card as the user drags the cursor
     const motionValue = useMotionValue(0);
-
-    // To rotate the card as the card moves on drag
-    const rotateValue = useTransform(motionValue, [-300, 300], [-50, 50]);
+    const opacityValue = useTransform(motionValue, [-300, -150, 0, 150, 300], [0, 1, 1, 1, 0]);
     
-    const opacityValue = useTransform(
-        motionValue,
-        [-300, -150, 0, 150, 300],
-        [0, 1, 1, 1, 0]
-    );
-    
-    // Framer animation hook
     const animControls = useAnimation();
 
     const style = {
         background: "white",
         height: "50vmin",
         width: "30vmin",
-        display: "flex",
+        display: display ? "flex" : "none",
         flexDirection: "column",
         padding: "4vmin",
         borderRadius: "2vmin",
@@ -52,41 +43,34 @@ export const Card = ({ user }) => {
         style={style}
         drag="x"
         x={motionValue}
-        rotate={rotateValue}
         opacity={opacityValue}
         dragConstraints={{ left: -300, right: 300 }}
         onDragEnd={(event, info) => {
             if (Math.abs(info.offset.x) <= 150) {
-                animControls.start({ x: 0 });
+                animControls.set({ x: 0 });
             }
             else if(info.offset.x < 150) { // left
+                setDisplay(false);
                 likeUser(user.id);
-                animControls.start({ x: info.point.x < 0 ? -300 : 300 });
             }
-            else if(info.offset.x < -150) { // right
-                animControls.start({ x: info.point.x < 0 ? -300 : 300 });
+            else if(info.offset.x > -150) { // right
+                setDisplay(false);
             }
         }}>
-
-        <h1>{user.username}</h1>
-
-        <div>
-            <a href={"/find"}>Overview</a>
-        </div>
-
-        <img src={user.photoUrl} alt=""/>
-
-        <div>
-            {
-                user.gender === "female" 
-                ? <i className="fa fa-female" aria-hidden="true"></i>
-                : <i className="fa fa-male" aria-hidden="true"></i>
-            }
-            &nbsp;{user.age}
-        </div>
-
-        <p>{user.introduction}</p>
-
+            <h1>{user.username}</h1>
+            <div>
+                <a href={"/find"}>Overview</a>
+            </div>
+            <img src={user.photoUrl} alt=""/>
+            <div>
+                {
+                    user.gender === "female" 
+                    ? <i className="fa fa-female" aria-hidden="true"></i>
+                    : <i className="fa fa-male" aria-hidden="true"></i>
+                }
+                &nbsp;{user.age}
+            </div>
+            <p>{user.introduction}</p>
         </Frame>
     )
 }
