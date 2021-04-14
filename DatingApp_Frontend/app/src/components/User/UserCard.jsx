ff9a08d5f3042f35a748ecddd59ba3c8bf654c5c
EddyHamzich <poorgamerno1@gmail.com>
2021-04-14 08:16:34 +0200

    rewriting UserCard, using HD free portraits

--- DatingApp_Frontend/app/src/components/User/UserCard.jsx
+++ DatingApp_Frontend/app/src/components/User/UserCard.jsx
@@ -4,33 +4,10 @@ import './User.css';
 export const UserCard = ({ user, canBeMessaged, messageUser, canBeUnliked, unlikeUser }) => {
     return (
         <div className="user-info">
-        
-            <div className="user-nav">
-                { canBeUnliked && <div onClick={() => unlikeUser()}>
-                    <i className="fas fa-heart-broken"></i>
-                </div> }
-
-                <h2>{user.username}</h2>
-
-                { canBeMessaged && <div onClick={() => messageUser()}>
-                    <i className="far fa-comment"></i>
-                </div> }
-            </div>
 
             <img className="user-img" src={user.photoUrl} alt="" />
-
-            <div className="age-gender-box">
-                {
-                    user.gender === "female" 
-                    ? <i className="fa fa-female" aria-hidden="true"></i>
-                    : <i className="fa fa-male" aria-hidden="true"></i>
-                }
-                &nbsp;{user.age}
-            </div>
-
-            <p className="user-introduction">
-                {user.introduction}
-            </p>
+            <p className="user-name">{user.username} , {user.age}</p>
+            <p className="user-desc">{user.introduction}</p>
 
         </div>
     )
