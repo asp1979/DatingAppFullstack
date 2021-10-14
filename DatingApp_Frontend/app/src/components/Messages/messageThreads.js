51916a96891d357028ff046aecc81a1dd9684eaf
EddyHamzich <poorgamerno1@gmail.com>
2021-10-14 20:58:05 +0200

    optimize messageThreads algorithm

--- DatingApp_Frontend/app/src/components/Messages/messageThreads.js
+++ DatingApp_Frontend/app/src/components/Messages/messageThreads.js
@@ -1,24 +1,27 @@
-// this groups messages for each unique sender
+// this groups messages between 2 people (conversation thread)
+
 export function messageThreads(inbox, outbox) {
 
-    function groupBy(list, keyGetter) {
+    function groupBy(arr, keyGetter) {
         let map = new Map();
-        list.forEach((item) => {
+        arr.forEach((item) => {
             let key = keyGetter(item);
-            let collection = map.get(key);
-            if (!collection) {
+            let pair = map.get(key);
+            if(!pair) {
                 map.set(key, [item]);
             } else {
-                collection.push(item);
+                pair.push(item);
             }
         });
         return map;
     }
 
     let threads = [...groupBy(inbox, msg => msg.senderID)];
-    threads.forEach(x => x.push(x[1][0].senderUsername));
-    threads.forEach(x => x.push(x[1][0].senderPhotoUrl));
-    threads.forEach(x => x.push(x[1].reduce((a,x) => x.isRead ? a + 0 : a + 1, 0)));
+    threads.forEach(x => { 
+        x.push(x[1][0].senderUsername)
+        x.push(x[1][0].senderPhotoUrl)
+        x.push(x.push(x[1].reduce((a,x) => x.isRead ? a + 0 : a + 1, 0)));
+    });
 
     // threads[0][0] is the senderID 
     // threads[0][1] is all the message objects of the user
@@ -28,8 +31,10 @@ export function messageThreads(inbox, outbox) {
 
     let noReplyThreads = outbox.filter(msg => threads.every(x => x[0] !== msg.recipientID));
     noReplyThreads = [...groupBy(noReplyThreads, msg => msg.recipientID)];
-    noReplyThreads.forEach(x => x.push(x[1][0].recipientUsername));
-    noReplyThreads.forEach(x => x.push(x[1][0].recipientPhotoUrl));
+    noReplyThreads.forEach(x => {
+        x.push(x[1][0].recipientUsername)
+        x.push(x[1][0].recipientPhotoUrl)
+    });
 
     // same format as threads
     
